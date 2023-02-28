15050bc8ede3fabc82bf9d972f44ea7722cbdc1f
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-02-28 14:14:54 +0300

    add function Newarray

--- task/Program.cs
+++ task/Program.cs
@@ -3,3 +3,18 @@ Console.WriteLine("Enter an array elements separated by SPASE end press 'ENTER':
 string s = Console.ReadLine();
 char[] separator = new char[] {' '};
 array = s.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+void Newarray(string[] newarray)
+{
+    Console.WriteLine("New array:");
+  
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            newarray[i] = array[i];
+            Console.Write(newarray[i] + "  ");
+        }
+    }
+}
+Console.WriteLine();
+Newarray(array);
