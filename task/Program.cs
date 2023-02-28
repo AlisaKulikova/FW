f25b3def3dc8bc7eb1b65afcbe3b876aeaef557a
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-02-28 14:13:23 +0300

    add input parametrs

---
+++ task/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] array = { };
+Console.WriteLine("Enter an array elements separated by SPASE end press 'ENTER':");
+string s = Console.ReadLine();
+char[] separator = new char[] {' '};
+array = s.Split(separator, StringSplitOptions.RemoveEmptyEntries);
